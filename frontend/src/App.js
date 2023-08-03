9b6aa1e36ea833024cea1ab460804ea17b70d241
Hisham Akmal <hisham0502@gmail.com>
2023-08-03 16:56:23 +0530

    Frontend logic modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, BrowserRouter, Link } from "react-router-dom";
+
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route path="/" element={<Fib />} />
+          <Route path="otherpage" element={<OtherPage />} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/Fib.js
+++ frontend/src/Fib.js
@@ -16,10 +16,54 @@ export default Fib = () => {
 
   useEffect(() => {
     async function func2() {
-      const seenIndexes = await fetch("/api/values/current");
+      const seenIndexes = await fetch("/api/values/all");
       setIndexes(seenIndexes);
     }
 
     func2();
   });
+
+  function renderVals() {
+    const entries = [];
+    for (const key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} Fibonacci value is {values[key]}
+        </div>
+      );
+    }
+    return entries;
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("/api/values", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ index }),
+    });
+    setIndex("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter the index:</label>
+        <input
+          type="text"
+          value={index}
+          onChange={(e) => setIndex(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h3>Indexes computed:</h3>
+      {indexes.map(({ number }) => number).join(",")}
+
+      <h3>Calculated values:</h3>
+      {renderVals()}
+    </div>
+  );
 };
